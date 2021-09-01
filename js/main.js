e0baeb66452889846610755503b5fbf3e2322044
Mesalina23 <mebrapu23@gmail.com>
2021-09-01 11:01:24 +0200

    create JS data

--- js/main.js
+++ js/main.js
@@ -4,11 +4,36 @@ const check = document.querySelector ('.js_button');
 const userNumber = document.querySelector ('.js_userNumber');
 const clue = document.querySelector ('.js_clue');
 const counter = document.querySelector ('.js_counter');
+const randomNumber = getRandomNumber( 100) ;
+console.log (randomNumber);
 
-//2.creamos la función manejadora del evento:
+//3.a Definimos la función random:
+function getRandomNumber( max ) {
+    return Math.ceil (Math.random()*max);
+}
+
+//3.Definimos updateCheck:
+function updateCheck(){
+    if (userNumber.value <1 || userNumber.value>100){
+        clue.innerHTML = 'El ńumero ha de estar entre 1 y 100';
+    }else if(parseInt(userNumber.value) === randomNumber){
+        clue.innerHTML = 'Has ganado campeona!!!';
+    }else if (userNumber.value>randomNumber){
+        clue.innerHTML = 'Demasiado Alto';
+    }else if (userNumber.value < randomNumber){
+        clue.innerHTML = 'Demasiado Bajo';
+    }
+}
+//4.Definimos updateAttemps: 
+let cont = 0;
+function updateAttemps() {
+    cont++;
+    counter.innerHTML = `Número de intentos: ${cont}`;
+}
+//2.Creamos la función manejadora del evento:
 function handleUpdateCheck(){
     updateCheck ();
     updateAttemps (); 
 }
-//1.creamos el evento:
+//1.Creamos el evento:
 check.addEventListener('click', handleUpdateCheck);
