a6a9bb2157c3769f91808868bef99bdc485934d3
Mesalina23 <mebrapu23@gmail.com>
2021-09-01 09:56:24 +0200

    call elements from Html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+//llamamos a los elementos de HTML con los que vamos a trabajar:
+const check = document.querySelector ('.js_button');
+const userNumber = document.querySelector ('.js_userNumber');
+const clue = document.querySelector ('.js_clue');
+const counter = document.querySelector ('.js_counter');
