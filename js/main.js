7e2e7c5fa10a7b50b2fa8c4d9c1de76c2fe8d14a
Mesalina23 <mebrapu23@gmail.com>
2021-09-13 12:55:31 +0200

    add preventDefault

--- js/main.js
+++ js/main.js
@@ -1,34 +1,58 @@
 'use strict';
 //llamamos a los elementos de HTML con los que vamos a trabajar:
-const check = document.querySelector ('.js_button');
+const checkButton = document.querySelector ('.js_button');
 const userNumber = document.querySelector ('.js_userNumber');
-const clue = document.querySelector ('.js_clue');
-const counter = document.querySelector ('.js_counter');
+const clueParagraph = document.querySelector ('.js_clue');
+const counterParagraph = document.querySelector ('.js_counter');
 const randomNumber = getRandomNumber( 100) ;
 console.log (randomNumber);
+const mainForm = document.querySelector ('.js_mainForm');
+
+//5. Creamos un evento para eliminar el comportamiento por defecto del formulario:
+/*function handlePreventDefault (ev){
+    debugger;
+    if(ev.keyCode === 13){
+        ev.preventDefault ();
+        return false;
+    }
+    
+}
+userNumber.addEventListener ('key', handlePreventDefault);
+*/
+function cancelSubmit(ev) {
+    ev.preventDefault();
+}
+mainForm.addEventListener('submit', cancelSubmit)
 
 //3.a Definimos la función random:
 function getRandomNumber( max ) {
     return Math.ceil (Math.random()*max);
 }
 
+//3.b Definimos otra función que contendrá clue.innerHTML:
+function addContentClue (text){
+    clueParagraph.innerHTML = text;
+}
+
 //3.Definimos updateCheck:
 function updateCheck(){
-    if (userNumber.value <1 || userNumber.value>100){
-        clue.innerHTML = 'El ńumero ha de estar entre 1 y 100';
-    }else if(parseInt(userNumber.value) === randomNumber){
-        clue.innerHTML = 'Has ganado campeona!!!';
-    }else if (userNumber.value>randomNumber){
-        clue.innerHTML = 'Demasiado Alto';
-    }else if (userNumber.value < randomNumber){
-        clue.innerHTML = 'Demasiado Bajo';
+
+    const inputValue = parseInt (userNumber.value);
+    if (inputValue <1 || inputValue>100){
+        text  ('El ńumero ha de estar entre 1 y 100');
+    }else if(inputValue === randomNumber){
+        text ('Has ganado campeona!!!');
+    }else if (inputValue>randomNumber){
+        text ('Demasiado Alto');
+    }else if (inputValue < randomNumber){
+        text ('Demasiado Bajo');
     }
 }
 //4.Definimos updateAttemps: 
 let cont = 0;
 function updateAttemps() {
     cont++;
-    counter.innerHTML = `Número de intentos: ${cont}`;
+    counterParagraph.innerHTML = `Número de intentos: ${cont}`;
 }
 //2.Creamos la función manejadora del evento:
 function handleUpdateCheck(){
@@ -36,4 +60,4 @@ function handleUpdateCheck(){
     updateAttemps (); 
 }
 //1.Creamos el evento:
-check.addEventListener('click', handleUpdateCheck);
+checkButton.addEventListener('click', handleUpdateCheck);
